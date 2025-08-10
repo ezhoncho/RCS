376059f6ea1ef094bd43b4d5ac6a53e833ce73b2
ezhoncho <honchorusty@gmail.com>
2025-08-10 03:59:53 +0100

    Initial commit with project files for RCS

---
+++ mainwindow.h
@@ -0,0 +1,9 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <QMainWindow>
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = nullptr);
+};
+#endif
